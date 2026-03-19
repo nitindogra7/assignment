d49695777da4584e47b0a27639d98cfeb3aea84e
Nitin Dogra <dogranitin579@gmail.com>
2026-03-19 16:16:25 +0530

    urls

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://assignment-nu-nine-76.vercel.app"],
     credentials: true,
   })
 );
